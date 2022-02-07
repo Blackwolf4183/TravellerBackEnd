4896988110f30a8c490f97582ec7efebc6d8c375
Blackwolf4183 <aperezvic21@gmail.com>
2022-02-07 15:15:20 +0100

    dynamic port

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ moongose
     `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.dcpvz.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(
-    app.listen(5000, function () {
+    app.listen(process.env.PORT || 5000, function () {
       console.log("Express server listening on port 5000");
     })
   )
